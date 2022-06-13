aa79f37292afcc764bfc3af7f354a80e47af33e4
Rafael Oliveira <faeltwister@gmail.com>
2022-06-13 19:22:58 -0300

    aulas da alura

---
+++ aulas/constantes.js
@@ -0,0 +1,32 @@
+//op
+const meunumuero = 3;
+const num1 = 8;
+const num2 = 2;
+const num3 = 7;
+const num4 = 4.1;
+
+const operacao = num1 + num2;
+const multiplicacao = num1*num2;
+const flutuante = num3 * num4;
+const flut = num3 + num4;
+
+console.log(operacao);
+console.log("o numero é: ",multiplicacao);
+console.log("O resultado é: ", flut);
+console.log("O resultado é: ", flutuante);
+
+//strings
+
+const texto = "Hello world!";
+const texto2 = 'Olá mundo!';
+const senha = "Senha4434@zw!";
+const tique = '\u2705';
+const lol = 'U+26Fx';
+console.log(texto, texto2, senha);
+console.log(tique);
+console.log(lol);
+
+//bool
+
+
+
